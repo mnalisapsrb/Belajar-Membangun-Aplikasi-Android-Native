aef855f86aef96ffb5afc3c710c571acafbe5609
Monalisa Paulima Artha Pasaribu <monalisaapasaribu@gmail.com>
2018-01-19 10:36:05 +0700

    Create sampleIntent.java

---
+++ sampleIntent.java
@@ -0,0 +1,51 @@
+package com.example.monalisa.sample_intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSub1, btnSub2, btnDial;
+    private String strIntent;
+    private EditText textIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSub1 = (Button)findViewById(R.id.btn_activity_sub_1);
+        btnSub2 = (Button)findViewById(R.id.btn_activity_sub_2);
+        btnDial = (Button)findViewById(R.id.btn_activity_dial);
+
+        textIntent = (EditText)findViewById(R.id.text_Intent);
+
+        btnSub1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Sub1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSub2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                strIntent = textIntent.getText().toString();
+                Intent intent = new Intent(MainActivity.this, Sub2Activity.class);
+                intent.putExtra(Sub2Activity.KEY_DATA, strIntent);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:082166023688"));
+                startActivity(intent);
+            }
+        });
+    }
+}
